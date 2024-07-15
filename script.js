b38d8fb28172d606a4f4379b9f119736ff04869a
Trikumia <geroulakou.k@gmail.com>
2024-07-15 16:10:23 +0300

    update js file

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const wordA = ['puppies','babies','news'];
-const wordB = ['best','worst', 'lucky'];
-const wordC = ['Eat', 'Dance', 'Cry','Laugh'];
-const wordD = ['try', 'play', 'jump'];
+const wordObject = {
+    wordA : ['puppies','babies','news'],
+    wordB : ['best','worst', 'lucky'],
+    wordC : ['Eat', 'Dance', 'Cry','Laugh'],
+    wordD : ['try', 'play', 'jump']
+};
 
 const pickAword=() => {
-    let a = Math.floor(Math.random() * wordA.length);
-    a = wordA[a];
-    let b = Math.floor(Math.random() * wordB.length);
-    b = wordB[b];
-    let c = Math.floor(Math.random() * wordC.length);
-    c = wordC[c];
-    let d = Math.floor(Math.random() * wordD.length);
-    d = wordD[d];
+    let a = Math.floor(Math.random() * wordObject.wordA.length);
+    a = wordObject.wordA[a];
+    let b = Math.floor(Math.random() * wordObject.wordB.length);
+    b = wordObject.wordB[b];
+    let c = Math.floor(Math.random() * wordObject.wordC.length);
+    c = wordObject.wordC[c];
+    let d = Math.floor(Math.random() * wordObject.wordD.length);
+    d = wordObject.wordD[d];
 
     console.log('Happy ' + a + ' in on its way!');
     console.log('Today is your ' +b+ ' day!');
@@ -19,4 +21,5 @@ const pickAword=() => {
     console.log(d+ ' and fail, but never fail to ' +d);
 }
 
-pickAword();
+console.log('My Message Generator');
+pickAword(); 
